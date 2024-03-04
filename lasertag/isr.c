0ca52a55db401d81423d8060d8fd492169d0239e
Seth Jensen <seth@zayda.net>
2024-03-04 15:42:30 -0700

    Add transmitter to isr.c file

--- lasertag/isr.c
+++ lasertag/isr.c
@@ -1,7 +1,8 @@
 #include "isr.h"
-#include "trigger.h"
 #include "hitLedTimer.h"
 #include "lockoutTimer.h"
+#include "transmitter.h"
+#include "trigger.h"
 
 // The interrupt service routine (ISR) is implemented here.
 // Add function calls for state machine tick functions and
@@ -9,14 +10,16 @@
 
 // Perform initialization for interrupt and timing related modules.
 void isr_init() {
-    trigger_init();
-    hitLedTimer_init();
-    lockoutTimer_init();
+  trigger_init();
+  hitLedTimer_init();
+  lockoutTimer_init();
+  transmitter_init();
 }
 
 // This function is invoked by the timer interrupt at 100 kHz.
 void isr_function() {
-    trigger_tick();
-    hitLedTimer_tick();
-    lockoutTimer_tick();
+  trigger_tick();
+  hitLedTimer_tick();
+  lockoutTimer_tick();
+  transmitter_tick();
 }
